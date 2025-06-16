4a77ad3edc004e4e0170355580996e3380c7ca00
KaigeZheng <2687935764@qq.com>
2025-06-16 15:33:53 +0000

    Modity a bug

--- exp.sh
+++ exp.sh
@@ -16,27 +16,27 @@ run_script ./scripts/ECL_script/Informer.sh
 run_script ./scripts/ECL_script/Reformer.sh
 run_script ./scripts/ECL_script/Transformer.sh
 run_script ./scripts/ECL_script/DLinear.sh
-run_script ./scripts/ECL_script/PathTST.sh
+run_script ./scripts/ECL_script/PatchTST.sh
 
 run_script ./scripts/Exchange_script/Autoformer.sh
 run_script ./scripts/Exchange_script/Informer.sh
 run_script ./scripts/Exchange_script/Reformer.sh
 run_script ./scripts/Exchange_script/Transformer.sh
 run_script ./scripts/Exchange_script/DLinear.sh
-run_script ./scripts/Exchange_script/PathTST.sh
+run_script ./scripts/Exchange_script/PatchTST.sh
 
 run_script ./scripts/Weather_script/Autoformer.sh
 run_script ./scripts/Weather_script/Informer.sh
 run_script ./scripts/Weather_script/Reformer.sh
 run_script ./scripts/Weather_script/Transformer.sh
 run_script ./scripts/Weather_script/DLinear.sh
-run_script ./scripts/Weather_script/PathTST.sh
+run_script ./scripts/Weather_script/PatchTST.sh
 
 run_script ./scripts/Traffic_script/Autoformer.sh
 run_script ./scripts/Traffic_script/Informer.sh
 run_script ./scripts/Traffic_script/Reformer.sh
 run_script ./scripts/Traffic_script/Transformer.sh
 run_script ./scripts/Traffic_script/DLinear.sh
-run_script ./scripts/Traffic_script/PathTST.sh
+run_script ./scripts/Traffic_script/PatchTST.sh
 
 echo "All scripts attempted. Check above for success/failure."
